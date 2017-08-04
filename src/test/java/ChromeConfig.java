43c2330dc1a866cf4fcfc43d0130f8ffc73efb62
Robert Valek <rvalek@intersog.com>
2017-08-04 17:52:04 +0300

    stuff

--- src/test/java/ChromeConfig.java
+++ src/test/java/ChromeConfig.java
@@ -15,16 +15,16 @@ public class ChromeConfig {
     protected WebDriver driver;
 
     @BeforeTest
-    protected void startChrome(){
+    protected void startChrome()  throws Exception {
         System.setProperty("webdriver.chrome.driver","bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
         //options.setBinary("/usr/bin/google-chrome");
         //options.addArguments("--no-sandbox");
-        driver = new ChromeDriver(options);
+        //driver = new ChromeDriver(options);
 
-        // WebDriver driver = new RemoteWebDriver(
-        //                         new URL("http://localhost:9505/"), 
-        //                         DesiredCapabilities.chrome());
+        driver = new RemoteWebDriver(
+                                new URL("http://localhost:9505/"), 
+                                DesiredCapabilities.chrome());
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
